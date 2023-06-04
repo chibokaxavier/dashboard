757ecc64cb26548be6da2914955a8c17426247f0
manlikexavy <chibokaxavier@gmail.com>
2023-06-04 12:13:52 -0700

    HEADER COMPLETE, UP NEXT SIDEBAR

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='px-4 pt-4 flex justify-between'>
+        <h2>Dashboard</h2>
+        <h2>Welcome, Back Xavier</h2>
+
+    </div>
+  )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import Header from '@/components/Header'
 import Image from 'next/image'
 
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main className='bg-gray-100 min-h-screen'>
+      <Header/>
+    </main>
   )
 }
