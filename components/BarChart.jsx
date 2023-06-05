c9de5ac422da4b6a4d11da22e421e2641d752cc0
manlikexavy <chibokaxavier@gmail.com>
2023-06-05 01:46:52 -0700

    BARCHART COMPLETED, UP NEXT IS THE ORDERS

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useState } from "react";
-import { Chart } from "react-chartjs-2";
+import React, { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -25,10 +25,36 @@ const BarChart = () => {
     datasets: [],
   });
   const [chartOptions, setChartOptions] = useState({});
+  useEffect(() => {
+    setChartData({
+      labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      datasets: [
+        {
+          label: "Sales $",
+          data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+          borderColor: "rgb(53,235,100)",
+          backgroundColor: "rgb(53,235,100,0.4)",
+        },
+      ],
+    });
+    setChartOptions({
+        plugins: {
+            legend:{
+                position:'top',
+            },
+            title:{
+                display:true,
+                text:'Daily Revenue'
+            }
+        },
+        maintainAspectRatio:false,
+        responsive:true,
+    })
+  }, []);
   return (
     <>
       <div className="col-span-2 w-full relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
-        <Chart data={chartData} options={chartOptions} />
+        <Bar data={chartData} options={chartOptions} />
       </div>
     </>
   );
